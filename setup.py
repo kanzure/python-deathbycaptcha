9d17eec37b99c714c3d166ef5e2fcccacf6a19b0
Bryan Bishop <kanzure@gmail.com>
2012-11-26 00:18:31 -0600

    basic egg packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+version = "4.0.11"
+
+setup(name="deathbycaptcha",
+      version=version,
+      author="Sergey Kolchin",
+      author_email="ksa242@gmail.com",
+      # license="",
+      description="python API for deathbycaptcha.com",
+      url="https://github.com/kanzure/python-deathbycaptcha",
+      packages=find_packages())
